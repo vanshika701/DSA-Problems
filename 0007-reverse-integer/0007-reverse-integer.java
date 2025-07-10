47888c85845fe08a4852c712ba5273d61596b43b
Vanshika Srivastava <vs905@snu.edu.in>
2025-07-10 21:48:11 +0530

    Time: 1 ms (82.17%), Space: 40.7 MB (85.74%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int r=0;
+        while(x!=0){
+            int d=x%10;
+            if (r > Integer.MAX_VALUE / 10 || (r == Integer.MAX_VALUE / 10 && d > 7)) {
+                return 0;
+            }
+            if (r < Integer.MIN_VALUE / 10 || (r == Integer.MIN_VALUE / 10 && d < -8)) {
+                return 0;
+            }
+            r=r*10+d;
+            x=x/10;
+        }
+        return r;
+        
+    }
+}
