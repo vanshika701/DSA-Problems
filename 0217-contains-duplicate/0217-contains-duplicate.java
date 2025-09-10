b82cd25b57eaa59bcce1d48888e95eeee5be593d
Vanshika Srivastava <vs905@snu.edu.in>
2025-09-10 13:31:23 +0530

    Time: 35 ms (5.01%), Space: 60 MB (19.68%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>1)
+            return true;
+        }
+        return false;
+    }
+}
