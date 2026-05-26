864bed569924e6048691297b6990982f7e74ee08
Vanshika Srivastava <vs905@snu.edu.in>
2026-05-27 03:20:22 +0530

    Time: 19 ms (8.13%), Space: 46.5 MB (48.68%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        int p=0;
+        for(int i=0;i<ransomNote.length();i++){
+            char ch = ransomNote.charAt(i);
+
+    if(map.containsKey(ch)) {
+        map.put(ch, map.get(ch) + 1);
+    }
+    else {
+        map.put(ch, 1);
+        }
+        }
+    for(int i=0;i<magazine.length();i++){
+            char ch = magazine.charAt(i);
+
+    if(map.containsKey(ch)) {
+        map.put(ch, map.get(ch) - 1);
+    }
+        }
+    for(int val : map.values()) {
+    if(val > 0) {
+        return false;
+    }
+}
+        return true;
+    }
+
+}
