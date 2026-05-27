627b7f8043f38e852e51f93c4641cad5017fafc7
Vanshika Srivastava <vs905@snu.edu.in>
2026-05-28 02:23:48 +0530

    Time: 1 ms (99.91%), Space: 46.2 MB (79.42%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,30 +1,41 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        int p=0;
-        for(int i=0;i<ransomNote.length();i++){
-            char ch = ransomNote.charAt(i);
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         int p=0;
+//         for(int i=0;i<ransomNote.length();i++){
+//             char ch = ransomNote.charAt(i);
 
-    if(map.containsKey(ch)) {
-        map.put(ch, map.get(ch) + 1);
-    }
-    else {
-        map.put(ch, 1);
-        }
-        }
+//     if(map.containsKey(ch)) {
+//         map.put(ch, map.get(ch) + 1);
+//     }
+//     else {
+//         map.put(ch, 1);
+//         }
+//         }
+//     for(int i=0;i<magazine.length();i++){
+//             char ch = magazine.charAt(i);
+
+//     if(map.containsKey(ch)) {
+//         map.put(ch, map.get(ch) - 1);
+//     }
+//         }
+//     for(int val : map.values()) {
+//     if(val > 0) {
+//         return false;
+//     }
+// }
+//         return true;
+
+    int arr[]=new int[26];
     for(int i=0;i<magazine.length();i++){
-            char ch = magazine.charAt(i);
-
-    if(map.containsKey(ch)) {
-        map.put(ch, map.get(ch) - 1);
+        arr[magazine.charAt(i)-'a']+=1;
     }
-        }
-    for(int val : map.values()) {
-    if(val > 0) {
+    for(int i=0;i<ransomNote.length();i++){
+        arr[ransomNote.charAt(i)-'a']-=1;
+        if(arr[ransomNote.charAt(i)-'a']<0)
         return false;
     }
-}
-        return true;
+    return true;
     }
 
 }
