6351c8b9ad6a07aa818208ea11ee3154763d6c45
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-01 16:42:05 +0530

    Time: 11 ms (52.03%), Space: 117.3 MB (21.68%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.java
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.java
@@ -3,25 +3,28 @@ class Solution {
         int n=nums.length;
         int mini=Integer.MAX_VALUE;
         int maxi=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            boolean prime = true;
-            if(nums[i]==1)
-            prime =false;
-            int c=2;
-            while(c*c<=nums[i]){
-                if(nums[i]%c==0){
-                prime=false;
-                break;
-                }
-                c++;}
-                if(prime){
-                    if(i<mini)
+        boolean prime = true;
+            for(int i=0;i<n;i++)
+            {
+                if(isPrime(nums[i])){
+                    if(mini==Integer.MAX_VALUE)
                     mini=i;
-                    if(i>maxi)
-                    maxi=i;
+                maxi=i;
                 }
             }
         int dist=maxi-mini;
         return dist;
     }
+     private boolean isPrime(int num){
+            if(num<=1)
+            return false;
+            int c=2;
+            while(c*c<=num){
+                if(num%c==0){
+                return false;
+                }
+                c++;
+                }
+            return true;
+     }
 }
