e420e26d717e4616c38d55ca9b6b0c716f6b3d4d
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-12 21:02:10 +0530

    Time: 1 ms (80.15%), Space: 43.9 MB (9.62%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -18,6 +18,7 @@ class Solution {
         List<List<Integer>> result=new ArrayList<>();
         if(root==null)
         return result;
+        /*first method(also correct and optimal)
         Queue<TreeNode> queue=new LinkedList<>();
         queue.add(root);
         int l=0;
@@ -36,6 +37,34 @@ class Solution {
             if(l%2==0)
             Collections.reverse(level);
         result.add(level);
+        }*/
+        Deque<TreeNode> dequeue=new LinkedList();
+        dequeue.addFirst(root);
+        int l=1;
+        while(!dequeue.isEmpty()){
+            int levelSize=dequeue.size();
+            List<Integer> level=new ArrayList<>(levelSize);
+            for(int i=0;i<levelSize;i++){
+            if(l%2==1){
+            TreeNode curr=dequeue.removeFirst();
+            level.add(curr.val);
+            if(curr.left!=null)
+            dequeue.addLast(curr.left);
+            if(curr.right!=null)
+            dequeue.addLast(curr.right);
+            }
+            else
+            if(l%2==0){
+            TreeNode curr=dequeue.removeLast();
+            level.add(curr.val);
+            if(curr.right!=null)
+            dequeue.addFirst(curr.right);
+            if(curr.left!=null)
+            dequeue.addFirst(curr.left);
+            }
+            }
+            l++;
+            result.add(level);
         }
         return result;
     }
