6561be3309a95a4db5d05470c9d4373b5d54d85d
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-14 21:30:32 +0530

    Time: 1 ms (9.53%), Space: 43.6 MB (53.02%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while(!queue.isEmpty()){
+            int levelSize=queue.size();
+                TreeNode l=queue.poll();
+                TreeNode r=queue.poll();
+                if (l == null && r == null)
+                continue;
+
+                if (l == null || r == null)
+                return false;
+
+                if(l.val!=r.val)
+                return false;
+
+                        queue.add(l.left);
+                        queue.add(r.right);
+                        queue.add(l.right);
+                        queue.add(r.left);
+                }
+        return true;
+        }
+}//bfs lgake  phle que mei left ka left pusg kro nd right ka right, then left ka right and right ka left, nd que se do do member nikal ke check krte rho both shld 
