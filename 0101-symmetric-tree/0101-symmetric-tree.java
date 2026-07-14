57367790d8ec5039e91ae857f346f8103cf22bc7
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-14 21:35:58 +0530

    Time: 0 ms (100%), Space: 43.7 MB (34.16%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,29 +14,40 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null)
-    return true;
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
-        while(!queue.isEmpty()){
-                TreeNode l=queue.poll();
-                TreeNode r=queue.poll();
-                if (l == null && r == null)
-                continue;
+  public boolean isSymmetric(TreeNode root) {
+         if (root == null)
+     return true;
+     
+//         Queue<TreeNode> queue=new LinkedList<>();
+//         queue.add(root.left);
+//         queue.add(root.right);
+//         while(!queue.isEmpty()){
+//                 TreeNode l=queue.poll();
+//                 TreeNode r=queue.poll();
+//                 if (l == null && r == null)
+//                 continue;
 
-                if (l == null || r == null)
-                return false;
+//                 if (l == null || r == null)
+//                 return false;
 
-                if(l.val!=r.val)
-                return false;
+//                 if(l.val!=r.val)
+//                 return false;
 
-                        queue.add(l.left);
-                        queue.add(r.right);
-                        queue.add(l.right);
-                        queue.add(r.left);
-                }
-        return true;
-        }
-}//bfs lgake  phle que mei left ka left pusg kro nd right ka right, then left ka right and right ka left, nd que se do do member nikal ke check krte rho both shld be equal
+//                         queue.add(l.left);
+//                         queue.add(r.right);
+//                         queue.add(l.right);
+//                         queue.add(r.left);
+//                 }
+//         return true;
+//         }
+// }//bfs lgake  phle que mei left ka left pusg kro nd right ka right, then left ka right and right ka left, nd que se do do member nikal ke check krte rho both shld be equal
+return isMirror(root.left, root.right);
+  }
+boolean isMirror(TreeNode a, TreeNode b) {
+    if (a == null && b == null) return true;
+    if (a == null || b == null) return false;
+    return a.val == b.val
+        && isMirror(a.left, b.right)
+        && isMirror(a.right, b.left);
+}
+}
