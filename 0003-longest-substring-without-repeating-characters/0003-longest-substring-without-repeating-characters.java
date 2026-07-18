37958d07b5f9d7f81058e920bb1970eb379fec36
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-19 04:20:28 +0530

    Time: 6 ms (70.41%), Space: 45.2 MB (84.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,5 +1,9 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        //The brute force to this would be to generate all the strings and chcek which is the longest not repeating char strings
+        //in this the TC is O(N) becayse we traverse each element once 
+        //SC O(min(n,k)) wheee n is the total no of char in the string and k is the number of distinct char in the string. Because the smaller one will decide how many keys is rhe hashmap going to store
+        //map.getOrDefault(ch, 0)- this gets the value of the given key if its available in the hashmap and if its not available it gives 0 instead of giving null
         int n=s.length()-1;
         int l=0;
         int r=0;
