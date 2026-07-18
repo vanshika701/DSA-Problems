e1b7dea79018878bdc24136937617075486639a7
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-19 03:49:10 +0530

    Time: 2 ms (99.7%), Space: 61.5 MB (87.16%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
+        //so what we do is basically phle consider kro sare k cards ka sum starting se k cards se lelia and usko max manlo and then ek element ka sum remove krdo from the rear end of the initial subarray(at r) and r ko decraese krdo and summ mei ek element peeche se add krdo, and also decraese l--,  and tab tak kro ye jab tk r does not become 0
         int l=cardPoints.length-1;
         int r=k-1;
         int sum=0;
