20096c9819a982dbeb08bf136f4e5be02b7f475d
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-19 12:35:58 +0530

    Time: 3 ms (96.28%), Space: 52 MB (89.02%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,29 +1,53 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int l=0;
+        //So each element is processed at most:once when r enters it,once when l leaves it.
+        //Total operations are proportional to:n+n=2n
+        //Ignoring constants:O(n)
+	// ​int l=0;
+    //     int r=0;
+    //     int max=0;
+    //     int n=nums.length;
+        // while(r<n){
+        //     if(nums[r]==1)
+        //     {
+        //         r++;
+        //         max=Math.max(max,r-l);
+        //     }
+        //     else
+        //     {
+        //         if(k>0){
+        //         k--;
+        //         r++;
+        //         max=Math.max(max,r-l);
+        //         }
+        //         else
+        //         {
+        //             if(nums[l]==0)
+        //             k++;
+        //             l++;
+        //         }
+        //     }
+        // }
+        // return max;
+
+        //this soln has same tc nd sc
         int r=0;
         int max=0;
         int n=nums.length;
+        int l=0;
+        int zero=0;
         while(r<n){
-            if(nums[r]==1)
-            {
-                r++;
-                max=Math.max(max,r-l);
+            if(nums[r]==0){
+                zero++;
             }
-            else
-            {
-                if(k>0){
-                k--;
-                r++;
-                max=Math.max(max,r-l);
+            while(zero>k){
+                if(nums[l]==0){
+                    zero--;
                 }
-                else
-                {
-                    if(nums[l]==0)
-                    k++;
-                    l++;
-                }
+                l++;
             }
+            max=Math.max(max,r-l+1);
+            r++;
         }
         return max;
     }
