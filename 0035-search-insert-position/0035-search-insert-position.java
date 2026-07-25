b5ca2f54ba279306fae0c47c0c384dafe417737b
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-26 03:59:43 +0530

    Time: 0 ms (100%), Space: 45 MB (12.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>target||nums[i]==target){
-            return i;
+        int lower=0;
+        int end=nums.length-1;
+        int mid=0;
+        while(lower<=end){
+            mid=lower+(end-lower)/2;
+            if(nums[mid]==target)
+            return mid;
+            else
+            if(nums[mid]>target){
+            end=mid-1;
             }
-        }
-        return nums.length;
+            else{
+            lower=mid+1;
+            }
+            }
+            return lower;
     }
 }
