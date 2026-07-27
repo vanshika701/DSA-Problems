54548e783b793c369bb2b15942c8bc4ec0b49c62
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-28 03:34:55 +0530

    Time: 3 ms (99.85%), Space: 46.5 MB (26.31%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -74,7 +74,7 @@ class Solution {
 //     }
 // }
 
-
+//yaha bhi same O(1) complexiy hai both sc and tc, jabki do arrays use hue hai kyuki hmesha array ka size 13 hee rhega adn toh loop bhi 13 tk hee chlega
 int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
  StringBuilder ans = new StringBuilder("");
