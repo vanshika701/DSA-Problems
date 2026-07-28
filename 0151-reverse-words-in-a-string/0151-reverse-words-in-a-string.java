85ad9d02d4222d53c6f02117d3b2ad34866432a6
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-28 19:23:53 +0530

    Time: 3 ms (99.07%), Space: 44.1 MB (80.13%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -4,19 +4,20 @@ class Solution {
        int end=s.length()-1;
        StringBuilder sb=new StringBuilder("");
        while(i>=0){
-       while(i>=0&&s.charAt(i)==' '){
+       while(i>=0&&s.charAt(i)==' '){//tab tk decrease kro jb tk word na mil jaye taki sare rare spaces skip ho sake
         i--;
        }
        if (i < 0)
     break;
         end=i;
-       while(i>=0&&s.charAt(i)!=' '){
+       while(i>=0&&s.charAt(i)!=' '){//ab pura word extract krn eke lie tb tk chlao jb tk space na mil jaye, isse end and i index ke beech mei ek word pura extract ho jaega
         i--;
        }
        String str=s.substring(i+1,end+1);
        if (sb.length() > 0)
         sb.append(" ");
-       sb.append(str);
+       sb.append(str);//ab bass wo word string mei append krte jao taki last wala word sbse aage aajaye
+       //and ye if isliye hai taki last word ke baad space na aaye
        }
        return sb.toString();
     }
