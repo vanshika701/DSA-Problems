1ec9a49b6bfd37140a602424f3c993988c37d5a6
Vanshika Srivastava <vs905@snu.edu.in>
2026-07-31 05:04:37 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.59%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,14 +3,16 @@ class Solution {
         int l=s.length()-1;
         int r=l;
         int len=0;
-        while(r>=0&&s.charAt(r)==' '){
+        while(r>=0&&s.charAt(r)==' '){//since extra space bhi ho skti last mei like the second exacmple isiliye loop tab tak chalao jab sbse first word from right side nhi mil jata
             r--;
-            l=r;
+            l=r;//mark l as the ending of the last word
         }
-        while(r>=0&&s.charAt(r)!=' '){
+        while(r>=0&&s.charAt(r)!=' '){//now we will find the first letter index of that last word by looking for space after this word ends, and save it in r
            r--;
         }
-        len=l-r;
+        len=l-r;//word end -word start
         return len;
     }
+    //TC_O(N),SC_O(1);
+
 }
