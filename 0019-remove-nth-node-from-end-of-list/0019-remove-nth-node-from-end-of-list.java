04230d1e5a06b481c61af798cfc9e11f8f331712
Vanshika Srivastava <vs905@snu.edu.in>
2026-08-09 02:08:15 +0530

    Time: 0 ms (100%), Space: 43.5 MB (40.65%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,21 +10,53 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp=head;
-        int i=1;
-        while(temp.next!=null){
-            temp=temp.next;
-            i++;
+        //this node is not the most optimal solutoon for this because we had to traverse the list twice 
+        // ListNode temp=head;
+        // int i=1;
+        // while(temp.next!=null){
+        //     temp=temp.next;
+        //     i++;
+        // }
+        // if(i==n)
+        // return head.next;
+        // int k=1;
+        // temp=head;
+        // while(k!=(i-n)){
+        //    temp=temp.next;
+        //    k++;
+        // }
+        // temp.next=temp.next.next;
+        // return head;
+
+
+        //the optimal solution is when we traverse the list onl yonce
+        ListNode buffer=new ListNode(0);
+        buffer.next=head;
+        ListNode slow=buffer;
+        ListNode fast=buffer;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
         }
-        if(i==n)
-        return head.next;
-        int k=1;
-        temp=head;
-        while(k!=(i-n)){
-           temp=temp.next;
-           k++;
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
         }
-        temp.next=temp.next.next;
-        return head;
+        slow.next=slow.next.next;
+        return buffer.next;
     }
 }
+
+// ### Approach
+
+// Use the **two-pointer technique** with a dummy node.
+
+// * Create a dummy node before `head` to handle the case where the head itself needs to be removed.
+// * Move `fast` pointer `n` steps ahead of `slow`.
+// * Move both pointers together until `fast.next` becomes `null`. At this point, `slow` is positioned just before the node that needs to be removed.
+// * Remove the node using `slow.next = slow.next.next`.
+// * Return `dummy.next` as the new head.
+
+// ### Complexity
+
+// * **Time:** `O(L)`, where `L` is the length of the linked list.
+// * **Space:** `O(1)`.
