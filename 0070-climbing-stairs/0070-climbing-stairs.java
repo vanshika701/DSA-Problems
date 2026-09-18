620c765119eb77d9307d35169c2f53811c08434f
Vanshika Srivastava <vs905@snu.edu.in>
2026-09-19 02:59:56 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,12 @@
-class Solution {
-    public int climbStairs(int n) {
+// class Solution {
+//     public int climbStairs(int n) {
         //recursion
         // if(n==0)
         // return 1;
         // if(n==1)
         // return 1;
         // return climbStairs(n-1)+climbStairs(n-2);
+
         /*### Approach: Recursion
         To reach step `n`, the last move can either be
         * 1 step from `n-1`
@@ -20,23 +21,70 @@ class Solution {
         This is not a good soln because multiple subproblems are being solved again and again. therefore we ill do dp
         */
 
+
+
         //Memoization
+    // class Solution {
+    // public int climbStairs(int n) {
+    //     int[] dp=new int[n+1];
+    //     return solve(n, dp);
+    // }
+    //     public int solve(int n, int[] dp) {
+
+    //     if (n == 0)
+    //         return 1;
+
+    //     if (n == 1)
+    //         return 1;
+
+    //     if (dp[n] != 0)
+    //         return dp[n];
+
+    //     dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+    //     return dp[n];
+    //     }
+    // }
+    /*### Approach: Memoization
+The recursive solution has **overlapping subproblems**, meaning the same values are calculated repeatedly.
+To optimize it, we use a `dp` array where `dp[i]` stores the number of ways to reach step `i`.
+For each `n`, the last move can be either:
+* 1 step from `n-1`
+* 2 steps from `n-2`
+So we use:
+`ways(n) = ways(n-1) + ways(n-2)
+Before calculating, we check if `dp[n]` already contains the answer. If it does, we return it directly. Otherwise, we calculate the result, store it in `dp[n]`, and return it.
+**Time Complexity:** `O(n)` because each subproblem is calculated only once.
+**Space Complexity:** `O(n)` for the `dp` array and `O(n)` recursion stack, giving `O(n)` overall auxiliary space.
+*/
+
+
+//Tabulation
+class Solution {
+    public int climbStairs(int n) {
         int[] dp=new int[n+1];
-        return solve(n, dp);
-    }
-        public int solve(int n, int[] dp) {
-
-        if (n == 0)
-            return 1;
-
-        if (n == 1)
-            return 1;
-
-        if (dp[n] != 0)
-            return dp[n];
-
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-
-        return dp[n];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
+        return dp[n];
     }
+}
+// ### Approach: Tabulation
+
+// Instead of using recursion, we solve the problem **bottom-up** using a DP array.
+// `dp[i]` represents the number of ways to reach step `i`.
+
+// The base cases are:
+
+// * `dp[0] = 1`
+// * `dp[1] = 1`
+
+// For every step from `2` to `n`, the last move can be either 1 step or 2 steps, so:
+
+// `dp[i] = dp[i-1] + dp[i-2]`
+
+// Unlike memoization, tabulation does not use recursion. It directly calculates the DP states from the smallest subproblem to the final answer
+// **Time Complexity:** `O(n)` because we iterate from `2` to `n`.
+// **Space Complexity:** `O(n)` because we store the results in the `dp` array.
