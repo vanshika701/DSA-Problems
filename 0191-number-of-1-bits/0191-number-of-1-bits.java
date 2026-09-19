5384b72b7f338b59c05239d36461693356f0c0eb
Vanshika Srivastava <vs905@snu.edu.in>
2026-09-20 01:36:03 +0530

    Time: 0 ms (100%), Space: 42.4 MB (47.05%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,13 +1,12 @@
 class Solution {
     public int hammingWeight(int n) {
         int c=0;
-        int d=0;
-        while(n>0){
-            d=n%2;
-            if(d==1)
+       while(n>0){
+        if((n&1)==1){
             c++;
-            n=n/2;
         }
-        return c;
+        n=n>>1;
+       }
+       return c;
     }
 }
